948b416e2fe0b6208e7a9dc2717221e4507c8111
Olivier Ramier <olivier.ramier@gmail.com>
2020-02-02 21:11:56 +0100

    fix color mode :)

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -16,9 +16,7 @@ export const Wrapper: React.FC<WrapperProps> = ({ children, apolloClient }) => {
     <ApolloProvider client={apolloClient}>
       <ThemeProvider theme={theme}>
         <CSSReset />
-        <ColorModeProvider>
-          <Layout>{children}</Layout>
-        </ColorModeProvider>
+        <Layout>{children}</Layout>
         <style global jsx>
           {`
             html,
