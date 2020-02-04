f5c80bc2b3e893a4052584403fe0c3b9be47d25c
Olivier Ramier <olivier.ramier@gmail.com>
2020-02-04 22:40:26 +0100

    setup eslint to check gql

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+const schema = fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8').toString();
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'graphql/template-strings': [
+      'error',
+      {
+        // Import default settings for your GraphQL client. Supported values:
+        // 'apollo', 'relay', 'lokka', 'fraql', 'literal'
+        env: 'apollo',
+
+        // Import your schema JSON here
+        // schemaJson: require('./schema.json'),
+
+        // OR provide absolute path to your schema JSON (but not if using `eslint --cache`!)
+        // schemaJsonFilepath: path.resolve(__dirname, './schema.json'),
+
+        // OR provide the schema in the Schema Language format
+        schemaString: schema,
+
+        // tagName is gql by default
+        tagName: 'gql',
+      },
+    ],
+  },
+  plugins: ['graphql'],
+};
