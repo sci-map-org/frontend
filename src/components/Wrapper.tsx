0544973d174b6f38cf33b58cbf2750e23e430048
Olivier Ramier <olivier.ramier@gmail.com>
2020-08-12 00:13:29 +0200

    add meta title seo

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,8 +1,8 @@
-import { CSSReset, ThemeProvider } from '@chakra-ui/core';
-import React from 'react';
-import { NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { ApolloClient, ApolloProvider } from '@apollo/client';
-
+import { CSSReset, ThemeProvider } from '@chakra-ui/core';
+import { NormalizedCacheObject } from 'apollo-cache-inmemory';
+import { DefaultSeo } from 'next-seo';
+import React from 'react';
 import { Layout } from '../components/layout/Layout';
 import { theme } from '../theme/theme';
 import { UnauthentificatedModalProvider } from './auth/UnauthentificatedModal';
@@ -16,6 +16,7 @@ export const Wrapper: React.FC<WrapperProps> = ({ children, apolloClient }) => {
     <ApolloProvider client={apolloClient}>
       <ThemeProvider theme={theme}>
         <CSSReset />
+        <DefaultSeo title="Sci-Map.org" />
         <UnauthentificatedModalProvider>
           <Layout>{children}</Layout>
         </UnauthentificatedModalProvider>
