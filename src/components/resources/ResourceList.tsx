2a082bc32ba01f93ed883ee18071e69690d86853
Olivier Ramier <olivier.ramier@gmail.com>
2020-08-25 02:21:31 +0200

    show all resources on resource list page

--- src/components/resources/ResourceList.tsx
+++ src/components/resources/ResourceList.tsx
@@ -4,6 +4,7 @@ import gql from 'graphql-tag';
 import { ResourcePreviewData } from '../../graphql/resources/resources.fragments';
 import { useListDomainResourcePreviewsQuery } from './ResourceList.generated';
 import { ResourcePreview } from './ResourcePreview';
+import { DomainResourcesSortingType, SortingDirection } from '../../graphql/types';
 
 export const listDomainResourcePreviews = gql`
   query listDomainResourcePreviews($domainKey: String!, $options: DomainResourcesOptions!) {
@@ -25,12 +26,20 @@ interface ResourceListProps {
 }
 
 export const ResourceList: React.FC<ResourceListProps> = ({ domainKey }) => {
-  const { data } = useListDomainResourcePreviewsQuery({ variables: { domainKey, options: { pagination: {} } } });
+  const { data } = useListDomainResourcePreviewsQuery({
+    variables: {
+      domainKey,
+      options: {
+        pagination: {},
+        sorting: { type: DomainResourcesSortingType.CreationDate, direction: SortingDirection.Desc },
+      },
+    },
+  });
 
   return (
     <Box borderWidth={1} borderColor="gray.200" borderRadius={4} p={4} width="100%">
       {data?.getDomainByKey.resources?.items &&
-        data?.getDomainByKey.resources?.items.map(preview => (
+        data?.getDomainByKey.resources?.items.map((preview) => (
           <ResourcePreview key={preview._id} resourcePreview={preview} />
         ))}
     </Box>
