ad3562ae43b5c5ff321dc2eae7fc86c752c38e06
Olivier Ramier <olivier.ramier@gmail.com>
2020-09-08 20:45:02 +0200

    setup brand color (teal)

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -12,6 +12,21 @@ export const theme = extendTheme({
     current: 'currentColor',
     black: '#000',
     white: '#fff',
+    main: '#4FD1C5', // teal 300
+    mainDarker: '#38B2AC', // teal 400
+    brand: {
+      // (charka default teal)
+      50: '#E6FFFA',
+      100: '#B2F5EA',
+      200: '#81E6D9',
+      300: '#4FD1C5',
+      400: '#38B2AC',
+      500: '#319795',
+      600: '#2C7A7B',
+      700: '#285E61',
+      800: '#234E52',
+      900: '#1D4044',
+    },
     backgroundColor: {
       0: 'rgba(255, 255, 255)',
       1: 'rgba(253, 253, 253)',
