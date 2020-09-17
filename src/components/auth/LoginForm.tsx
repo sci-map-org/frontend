96281cb8d0b5b164d131e4b6cd06f834592a4561
Olivier Ramier <olivier.ramier@gmail.com>
2020-09-17 05:35:50 +0200

    improve login/register forms

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Input, Stack, Text } from '@chakra-ui/core';
+import { Box, Button, Divider, Input, Stack, Text } from '@chakra-ui/core';
 import { useState } from 'react';
 import { useLogin } from '../../graphql/users/users.hooks';
 import { PasswordInput } from '../input/PasswordInput';
@@ -19,7 +19,7 @@ export const LoginForm: React.FC<{
   });
   return (
     <Stack spacing={6} textAlign="center">
-      <Text fontSize="xl">Using Third Parties</Text>
+      <Text fontSize="xl">With your Google account</Text>
       <Stack spacing={2} textAlign="center" onClick={(e) => e.stopPropagation()}>
         <GoogleAuthButton
           buttonText="Login with Google"
@@ -31,8 +31,9 @@ export const LoginForm: React.FC<{
           onFailure={() => {}}
         />
       </Stack>
+      <Divider />
       <Stack spacing={2}>
-        <Text fontSize="xl">With Email and Password</Text>
+        <Text fontSize="xl">Or with your email and password</Text>
         <Input
           placeholder="Email"
           size="md"
