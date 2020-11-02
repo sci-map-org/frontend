4b99cfbcb6e923a14c4e8081b274f7a2dc5c6afc
Olivier Ramier <olivier.ramier@gmail.com>
2020-11-02 21:10:23 +0100

    plausible analytics

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -36,6 +36,7 @@ export const Wrapper: React.FC<WrapperProps> = ({ children, apolloClient }) => {
             }
           `}
         </style>
+        <script async defer data-domain="sci-map.org" src="http://analytics.sci-map.org/js/plausible.js"></script>
       </ChakraProvider>
     </ApolloProvider>
   );
