faa5ec7c4e773137509be5cab1b1d5c823434e2b
Olivier Ramier <olivier.ramier@gmail.com>
2020-11-12 14:35:26 +0100

    hide covered topics if none and can't edit

--- src/components/resources/LearningMaterialCoveredTopics.tsx
+++ src/components/resources/LearningMaterialCoveredTopics.tsx
@@ -11,18 +11,28 @@ export const LearningMaterialCoveredTopics: React.FC<{
   w?: StackProps['w'];
 }> = ({ w, learningMaterial, isLoading, editMode }) => {
   return (
+    <RoleAccess
+      accessRule={editMode}
+      renderAccessDenied={() =>
+        !!learningMaterial.coveredConceptsByDomain?.length && (
+          <CoveredTopicsContainer w={w}>
+            <LearningMaterialCoveredConceptsByDomainViewer learningMaterial={learningMaterial} isLoading={isLoading} />
+          </CoveredTopicsContainer>
+        )
+      }
+    >
+      <CoveredTopicsContainer w={w}>
+        <LearningMaterialDomainAndCoveredConceptsSelector learningMaterial={learningMaterial} isLoading={isLoading} />
+      </CoveredTopicsContainer>
+    </RoleAccess>
+  );
+};
+
+const CoveredTopicsContainer: React.FC<{ w?: StackProps['w'] }> = ({ w, children }) => {
+  return (
     <Stack w={w} backgroundColor="gray.100" px={4} mt={4} py={3} borderRadius={5}>
       <Heading size="sm">Covered Topics</Heading>
-      <RoleAccess
-        accessRule={editMode}
-        renderAccessDenied={() =>
-          learningMaterial.coveredConceptsByDomain?.length && (
-            <LearningMaterialCoveredConceptsByDomainViewer learningMaterial={learningMaterial} isLoading={isLoading} />
-          )
-        }
-      >
-        <LearningMaterialDomainAndCoveredConceptsSelector learningMaterial={learningMaterial} isLoading={isLoading} />
-      </RoleAccess>
+      {children}
     </Stack>
   );
 };
