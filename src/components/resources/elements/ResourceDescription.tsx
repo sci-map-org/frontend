89b9f9e6b9f0f314f94204754ccd7361f8c7993f
Olivier Ramier <olivier.ramier@gmail.com>
2021-01-05 21:52:34 +0100

    remove resource description max length

--- src/components/resources/elements/ResourceDescription.tsx
+++ src/components/resources/elements/ResourceDescription.tsx
@@ -17,19 +17,22 @@ export const ResourceDescriptionInput: React.FC<{
   value?: string;
   onChange: (value?: string) => void;
   maxLength?: number;
-}> = ({ value, onChange, maxLength = 1000 }) => {
+}> = ({ value, onChange, maxLength }) => {
   return (
-    <FormControl isInvalid={!!value && value.length > maxLength}>
+    <FormControl>
       <Textarea
+        isInvalid={!(!maxLength || (!!value && value.length <= maxLength))}
         id="description"
         placeholder="Description (optional)"
         size="md"
         value={value}
         onChange={(e) => onChange(e.target.value || undefined)}
       ></Textarea>
-      <FormHelperText textAlign="right" id="description-helper-text">
-        {value ? value.length : 0}/{maxLength}
-      </FormHelperText>
+      {maxLength && (
+        <FormHelperText textAlign="right" id="description-helper-text">
+          {value ? value.length : 0}/{maxLength}
+        </FormHelperText>
+      )}
     </FormControl>
   );
 };
