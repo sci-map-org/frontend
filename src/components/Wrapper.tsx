2e884699e58e940a8dc43913e5b705312b2a95bf
Olivier Ramier <olivier.ramier@gmail.com>
2021-01-21 16:49:00 +0100

    avenir cdn http -> https

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -22,7 +22,7 @@ export const Wrapper: React.FC<WrapperProps> = ({ children, apolloClient }) => {
         <style global jsx>
           {`
             @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
-            @import url('http://fonts.cdnfonts.com/css/avenir-next-lt-pro');
+            @import url('https://fonts.cdnfonts.com/css/avenir-next-lt-pro');
             html,
             body,
             body > div:first-child,
