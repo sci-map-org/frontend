c36dde7806bd9dabd259ca1d1e3cf203453a4909
Olivier Ramier <olivier.ramier@gmail.com>
2021-03-04 10:49:25 +0100

    lg circular progress dynamic size

--- src/components/learning_goals/LearningGoalCircularProgress.tsx
+++ src/components/learning_goals/LearningGoalCircularProgress.tsx
@@ -23,7 +23,16 @@ export const LearningGoalCircularProgress: React.FC<LearningGoalCircularProgress
   pxSize = 30,
 }) => {
   if (!learningGoal.progress) return null;
-  if (learningGoal.progress.level === 100) return <Icon as={IoIosCheckmarkCircle} boxSize={pxSize + 8 + 'px'} />;
+  if (learningGoal.progress.level === 100)
+    return (
+      //Some hacking to make it align with the circular progress (path in icon has a margin)
+      <Icon
+        as={IoIosCheckmarkCircle}
+        boxSize={pxSize + pxSize / 5 + 'px'}
+        mb={`${-Math.floor((1 / 8) * pxSize)}px`}
+        color={learningGoal.progress?.level === 100 ? 'white' : 'black'}
+      />
+    );
   return (
     <CircularProgress
       thickness="10px"
