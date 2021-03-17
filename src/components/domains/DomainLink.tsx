d57bfeef2dc13f25f3bacf1fb86bdd002ad93afd
Olivier Ramier <olivier.ramier@gmail.com>
2021-03-17 14:05:21 +0100

    improve domain link

--- src/components/domains/DomainLink.tsx
+++ src/components/domains/DomainLink.tsx
@@ -2,20 +2,20 @@ import { BreadcrumbLinkProps } from '@chakra-ui/react';
 import { LinkProps } from 'next/link';
 import { DomainLinkDataFragment } from '../../graphql/domains/domains.fragments.generated';
 import { DomainPageInfo } from '../../pages/RoutesPageInfos';
-import { PageLink } from '../navigation/InternalLink';
+import { PageLink, PageLinkProps } from '../navigation/InternalLink';
 
 export const domainLinkStyleProps: Omit<LinkProps & BreadcrumbLinkProps, 'href'> = {
   fontWeight: 700,
   color: 'gray.600',
 };
 
-interface DomainLinkProps {
+interface DomainLinkProps extends Omit<PageLinkProps, 'pageInfo'> {
   domain: DomainLinkDataFragment;
 }
-export const DomainLink: React.FC<DomainLinkProps> = ({ domain }) => {
+export const DomainLink: React.FC<DomainLinkProps> = ({ domain, children, ...props }) => {
   return (
-    <PageLink pageInfo={DomainPageInfo(domain)} {...domainLinkStyleProps}>
-      {domain.name}
+    <PageLink pageInfo={DomainPageInfo(domain)} {...domainLinkStyleProps} {...props}>
+      {children || domain.name}
     </PageLink>
   );
 };
