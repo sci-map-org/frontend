505f8fabdd505db790993d281bb01a8c4d7f0dc3
Olivier Ramier <olivier.ramier@gmail.com>
2021-04-01 14:48:47 +0200

    domain learning goals add + button

--- src/components/domains/DomainLearningGoals.tsx
+++ src/components/domains/DomainLearningGoals.tsx
@@ -1,5 +1,10 @@
-import { Flex, Stack, Text } from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons';
+import { Flex, Heading, IconButton, Stack, Text } from '@chakra-ui/react';
+import { DomainDataFragment } from '../../graphql/domains/domains.fragments.generated';
 import { LearningGoalBelongsToDomain } from '../../graphql/types';
+import { useCurrentUser } from '../../graphql/users/users.hooks';
+import { routerPushToPage } from '../../pages/PageInfo';
+import { AddLearningGoalToDomainPageInfo } from '../../pages/RoutesPageInfos';
 import { LearningGoalCard } from '../learning_goals/cards/LearningGoalCard';
 import { LearningGoalCardDataFragment } from '../learning_goals/cards/LearningGoalCard.generated';
 import { LearningGoalCardList } from '../learning_goals/cards/LearningGoalCardList';
@@ -11,22 +16,55 @@ interface DomainLearningGoalsProps {
       learningGoal: LearningGoalCardDataFragment;
     }
   >;
+  domain: DomainDataFragment;
   isLoading?: boolean;
 }
-export const DomainLearningGoals: React.FC<DomainLearningGoalsProps> = ({ learningGoalItems, isLoading }) => {
+export const DomainLearningGoals: React.FC<DomainLearningGoalsProps> = ({ domain, learningGoalItems, isLoading }) => {
+  const { currentUser } = useCurrentUser();
   return (
     <Flex direction="column" pb={5}>
-      <Stack direction="row" alignItems="center" mb={2}>
-        <LearningGoalIcon boxSize="22px" />
-        <Text fontSize="2xl">Learning Goals</Text>
+      <Stack direction="row" alignItems="center" mb={3}>
+        <LearningGoalIcon boxSize="24px" />
+        <Heading fontSize="2xl" fontWeight={600} color="blackAlpha.800">
+          Learning Goals
+        </Heading>
       </Stack>
 
-      <LearningGoalCardList
-        learningGoalItems={learningGoalItems}
-        spacing="20px"
-        isLoading={isLoading}
-        renderCard={({ learningGoal }, hover) => <LearningGoalCard learningGoal={learningGoal} mouseHover={hover} />}
-      />
+      <Flex direction="row">
+        <LearningGoalCardList
+          learningGoalItems={learningGoalItems}
+          spacing="20px"
+          isLoading={isLoading}
+          renderCard={({ learningGoal }, hover) => <LearningGoalCard learningGoal={learningGoal} mouseHover={hover} />}
+        />
+        {!!currentUser && (
+          <Flex direction="row" alignItems="stretch" borderLeftWidth={0} ml={1} borderColor="gray.400">
+            <Flex
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+              px={8}
+              ml={4}
+              borderWidth={1}
+              borderColor="teal.600"
+              borderRadius={4}
+              mb={2}
+            >
+              <IconButton
+                size="lg"
+                aria-label="add goal"
+                variant="ghost"
+                icon={<AddIcon />}
+                isRound
+                colorScheme="teal"
+                onClick={() => {
+                  routerPushToPage(AddLearningGoalToDomainPageInfo(domain));
+                }}
+              />
+            </Flex>
+          </Flex>
+        )}
+      </Flex>
     </Flex>
   );
 };
