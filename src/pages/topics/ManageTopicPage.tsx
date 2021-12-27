41f945d9af8377b2e4269d7c05627b70fbb768b1
Olivier Ramier <olivier.ramier@gmail.com>
2021-12-28 00:58:35 +0100

    manage topic page tab query param

--- src/pages/topics/ManageTopicPage.tsx
+++ src/pages/topics/ManageTopicPage.tsx
@@ -1,19 +1,32 @@
 import { Image } from '@chakra-ui/react';
-import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useMemo } from 'react';
 import { PageLayout } from '../../components/layout/PageLayout';
 import { ManageTopic, ManageTopicTabIndex } from '../../components/topics/ManageTopic';
 import { useGetTopicByKeyManageTopicPageQuery } from '../../components/topics/ManageTopic.generated';
 import { NotFoundPage } from '../NotFoundPage';
-import { ManageTopicPageInfo, TopicPageInfo } from '../RoutesPageInfos';
+import { ManageTopicPageInfo, ManageTopicPagePath, TopicPageInfo } from '../RoutesPageInfos';
 
 export const ManageTopicPage: React.FC<{ topicKey: string }> = ({ topicKey }) => {
-  // TODO: tab in Url
-  const [tab, setTab] = useState(ManageTopicTabIndex.Data);
+  const router = useRouter();
+  const tabQueryParam = useMemo(() => {
+    if (router.query.tab && typeof router.query.tab === 'string') {
+      try {
+        const parsedTab = parseInt(router.query.tab, 10);
+        if (parsedTab === ManageTopicTabIndex.Data) return ManageTopicTabIndex.Data;
+        if (parsedTab === ManageTopicTabIndex.SubTopics) return ManageTopicTabIndex.SubTopics;
+        return null;
+      } catch (err) {
+        return null;
+      }
+    }
+    return null;
+  }, [router.query.tab]);
+
   const { data, loading, refetch } = useGetTopicByKeyManageTopicPageQuery({
     variables: { topicKey },
   });
   if (!data && !loading) return <NotFoundPage />;
-
   const topic = data?.getTopicByKey;
   return (
     <PageLayout
@@ -30,7 +43,25 @@ export const ManageTopicPage: React.FC<{ topicKey: string }> = ({ topicKey }) =>
         />
       }
     >
-      {topic && <ManageTopic topic={topic} tab={tab} onChangeTab={setTab} refetch={refetch} />}
+      {topic && (
+        <ManageTopic
+          topic={topic}
+          tab={tabQueryParam || ManageTopicTabIndex.Data}
+          onChangeTab={(newTab) => {
+            router.push(
+              {
+                pathname: ManageTopicPagePath(topicKey),
+                query: {
+                  tab: newTab,
+                },
+              },
+              undefined,
+              { shallow: true }
+            );
+          }}
+          refetch={refetch}
+        />
+      )}
     </PageLayout>
   );
 };
