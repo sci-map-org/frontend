2112f6ed94cad6a48efc3a2ab0b3d659aaca6c64
Olivier Ramier <olivier.ramier@gmail.com>
2022-01-25 20:29:30 +0100

    increase topic description char limit

--- src/components/topics/fields/TopicDescription.tsx
+++ src/components/topics/fields/TopicDescription.tsx
@@ -112,7 +112,7 @@ export const pullTopicDescriptions = gql`
   }
 `;
 
-export const TOPIC_DESCRIPTION_MAX_LENGTH = 500;
+export const TOPIC_DESCRIPTION_MAX_LENGTH = 1000;
 
 export const TopicDescriptionField: React.FC<{
   value?: string | null;
