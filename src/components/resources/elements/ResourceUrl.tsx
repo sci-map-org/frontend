0a665e62157a3e370d8719fd24ec1afa848c9cb5
Olivier Ramier <olivier.ramier@gmail.com>
2022-02-11 00:21:42 +0100

    handle already existing error

--- src/components/resources/elements/ResourceUrl.tsx
+++ src/components/resources/elements/ResourceUrl.tsx
@@ -1,8 +1,13 @@
 import { ExternalLinkIcon } from '@chakra-ui/icons';
 import {
-  FormControl,
-  FormLabel,
-  IconButton,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  Center, IconButton,
   Input,
   InputGroup,
   InputRightElement,
@@ -11,18 +16,22 @@ import {
   Skeleton,
   Stack,
   Text,
-  TextProps,
+  TextProps
 } from '@chakra-ui/react';
 import gql from 'graphql-tag';
-import { useEffect } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { BeatLoader } from 'react-spinners';
+import { ResourceDataFragment } from '../../../graphql/resources/resources.fragments.generated';
 import {
   useAnalyzeResourceUrlLazyQuery,
-  useSetResourceOpenedMutation,
+  useSetResourceOpenedMutation
 } from '../../../graphql/resources/resources.operations.generated';
 import { AnalyzeResourceUrlResult } from '../../../graphql/types';
+import { routerPushToPage } from '../../../pages/PageInfo';
+import { ResourcePageInfo } from '../../../pages/RoutesPageInfos';
 import { toUrlPreview, validateUrl } from '../../../services/url.service';
 import { theme } from '../../../theme/theme';
+import { PageLink } from '../../navigation/InternalLink';
 import { ResourceUrlDataFragment } from './ResourceUrl.generated';
 
 export const ResourceUrlData = gql`
@@ -99,51 +108,115 @@ interface ResourceUrlInputProps {
   onAnalyzed?: (resourceData: AnalyzeResourceUrlResult) => void;
 }
 export const ResourceUrlInput: React.FC<ResourceUrlInputProps> = ({ value, onChange, analyze, onAnalyzed }) => {
-  const isValidUrl = validateUrl(value);
+  const isValidUrl = useMemo(() => validateUrl(value), [value]);
+  const [existingResource, setExistingResource] = useState<ResourceDataFragment>();
+
+  const closeAlertDialog = useCallback(() => {
+    onChange('');
+    setExistingResource(undefined);
+  }, [])
+
+  const cancelRef = useRef<HTMLButtonElement>(null);
 
   const [analyzeResourceUrl, { loading }] = useAnalyzeResourceUrlLazyQuery({
     fetchPolicy: 'no-cache',
     onCompleted(data) {
+      setExistingResource(undefined);
       onAnalyzed && onAnalyzed(data.analyzeResourceUrl);
     },
+    onError(err) {
+      const errorCodes = err.graphQLErrors.map((gqlErr) => gqlErr.extensions.code);
+      const existingResourceErrorIndex = errorCodes.indexOf('RESOURCE_ALREADY_EXISTS');
+      if (existingResourceErrorIndex > -1) {
+        const error = err.graphQLErrors[existingResourceErrorIndex];
+        if (!error.extensions.existingResource)
+          throw new Error('existingResource not supplied in RESOURCE_ALREADY_EXISTS error');
+        setExistingResource(error.extensions.existingResource);
+      }
+    },
   });
+  
 
   useEffect(() => {
     if (!!value && analyze && isValidUrl) analyzeResourceUrl({ variables: { url: value } });
   }, [value]);
   return (
-    <InputGroup>
-      <Input
-        id="url"
-        isInvalid={!!value && !isValidUrl}
-        placeholder="https://example.com"
-        size="md"
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-      ></Input>
-      <InputRightElement w="auto" display="flex" justifyContent="center">
-        <Stack direction="row" align="center" mr={2}>
-          {loading && <BeatLoader size={8} margin={2} color={theme.colors.main} />}
+    <>
+      <InputGroup>
+        <Input
+          isInvalid={!!value && !isValidUrl}
+          placeholder="https://example.com"
+          size="md"
+          value={value}
+          {...(!!value &&
+            isValidUrl && {
+              color: 'blue.500',
+              textDecoration: 'underline',
+            })}
+          onChange={(e) => onChange(e.target.value)}
+        ></Input>
+        <InputRightElement w="auto" display="flex" justifyContent="center">
+          <Stack direction="row" align="center" mr={2}>
+            {loading && <BeatLoader size={8} margin={2} color={theme.colors.main} />}
 
-          {value && (
-            <Link
-              href={value}
-              isExternal
-              onClick={(e) => {
-                if (!isValidUrl) e.preventDefault(); // Not good for accessibility. Refactor to entirely remove link
-              }}
-            >
-              <IconButton
-                isDisabled={!isValidUrl}
-                size="xs"
-                aria-label="Open link"
-                color={isValidUrl ? 'green.400' : 'red.400'}
-                icon={<ExternalLinkIcon />}
-              />
-            </Link>
-          )}
-        </Stack>
-      </InputRightElement>
-    </InputGroup>
+            {value && (
+              <Link
+                href={value}
+                isExternal
+                onClick={(e) => {
+                  if (!isValidUrl) e.preventDefault(); // Not good for accessibility. Refactor to entirely remove link
+                }}
+              >
+                <IconButton
+                  isDisabled={!isValidUrl}
+                  size="xs"
+                  aria-label="Open link"
+                  variant="ghost"
+                  color={isValidUrl ? 'blue.500' : 'red.400'}
+                  icon={<ExternalLinkIcon />}
+                />
+              </Link>
+            )}
+          </Stack>
+        </InputRightElement>
+      </InputGroup>
+
+      {existingResource && (
+        <AlertDialog isOpen={!!existingResource} leastDestructiveRef={cancelRef} onClose={closeAlertDialog}>
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                Resource already exists
+              </AlertDialogHeader>
+
+              <AlertDialogBody display="flex" flexDir="column" alignItems="stretch">
+                <Text textAlign="center" fontWeight={500}>A resource with the url <br /><ResourceUrlLink resource={existingResource} maxLength={40}/> already exists</Text>
+                <Center py={8}>
+                <PageLink pageInfo={ResourcePageInfo(existingResource)} fontWeight={600} fontSize="lg"  color="gray.700" isExternal>{existingResource.name}<ExternalLinkIcon ml={1} /></PageLink>
+                </Center>
+              </AlertDialogBody>
+
+              <AlertDialogFooter>
+                <Stack direction="row"> 
+                <Button
+                  ref={cancelRef}
+                  onClick={closeAlertDialog}
+                  variant="outline"
+                >
+                  Close
+                </Button>
+                <Button
+                  onClick={() => routerPushToPage(ResourcePageInfo(existingResource))}
+                  colorScheme="blue"
+                >
+                  Go to existing Resource
+                </Button></Stack>
+
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
+      )}
+    </>
   );
 };
