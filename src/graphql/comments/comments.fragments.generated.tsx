0035d8ee2d7da00a882d5d3310d6c03bfb73d3a4
Olivier Ramier <olivier.ramier@gmail.com>
2022-04-01 15:09:36 +0200

    remove comment fragments unused

--- src/graphql/comments/comments.fragments.generated.tsx
+++
@@ -1,3 +0,0 @@
-import * as Types from '../types';
-
-export type CommentFullDataFragment = { __typename?: 'Comment', _id: string, discussionId: string, parentId?: string | null | undefined, contentMarkdown: string, postedAt: string, postedByUserId: string };
