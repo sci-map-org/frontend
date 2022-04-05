5408ca01efc29a1d2e586506beab9621498fc916
Olivier Ramier <olivier.ramier@gmail.com>
2022-04-05 14:35:35 +0200

    fix ?

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,8 +2,12 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  if (process.env.NODE_ENV === 'production' && req.headers.get('x-forwarded-proto') !== 'https') {
-    return NextResponse.redirect(`https://${req.headers.get('host')}${req.page.name}`);
+  if (
+    process.env.NODE_ENV === 'production' &&
+    req.headers.get('x-forwarded-proto') !== 'https' &&
+    req.page.name !== '/api/healthcheck'
+  ) {
+    return NextResponse.redirect(req.url.replace('http://', 'https://'));
   } else {
     NextResponse.next();
   }
