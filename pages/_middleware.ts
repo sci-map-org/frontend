b390513d4e9234530baa9e831b7298719de0af8c
Olivier Ramier <olivier.ramier@gmail.com>
2022-04-05 14:54:22 +0200

    fix ??

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -7,7 +7,7 @@ export function middleware(req: NextRequest) {
     req.headers.get('x-forwarded-proto') !== 'https' &&
     req.page.name !== '/api/healthcheck'
   ) {
-    return NextResponse.redirect(req.url.replace('http://', 'https://'));
+    return NextResponse.redirect(`https://${req.headers.get('host')}${req.nextUrl.pathname}${req.nextUrl.search}`);
   } else {
     NextResponse.next();
   }
